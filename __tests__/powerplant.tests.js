ad7976ae1c9bb1caccf3a697aab45481727332af
Alex Shevlin <alexshevlin1@gmail.com>
2022-08-30 16:36:34 -0700

    update state to recieve imported class.
    Co-authored-by: Seung Lee <leark@users.noreply.github.com>

--- __tests__/powerplant.tests.js
+++ __tests__/powerplant.tests.js
@@ -1,22 +1,30 @@
-import { stateControl, storeState, photosynthesis, feedWater, giveF } from '../src/js/powerplant.js';
+import { storeState, photosynthesis, feedWater, giveF } from '../src/js/powerplant.js';
 
-describe('PowerPlant', () => {
-  beforeEach(() => {
-    const stateControl = storeState();
-  });
-
+describe('powerplant', () => {
+  const plant = { soil: 0, light: 0, water: 0, attack: 0, health: 0 };
+  
   test('Increase light by 5', () => {
-    const newState = stateControl(photosynthesis(5));
-    expect(stateControl()).toEqual({light: 5});
+    const plant1 = storeState(plant);
+    plant1(photosynthesis(5));
+    expect(plant1().light).toEqual(5);
   });
   
   test('Increase water by 3', () => {
-    const newState = stateControl(feedWater(3));
-    expect(stateControl()).toEqual({light: 5, water: 3});
+    const plant1 = storeState(plant);
+    plant1(feedWater(3));
+    expect(plant1().water).toEqual(3);
   });
 
   test('Increase soil by 4', () => {
-    const newState = stateControl(giveF(4));
-    expect(stateControl()).toEqual({light: 5, soil: 4, water: 3});
-  });
+    const plant1 = storeState(plant);
+    plant1(giveF(5));
+    expect(plant1().soil).toEqual(5);
+});
+
+  // test('has light:0 and soil: 0', () => {
+  //   let x = {light: 0, soil: 0};
+  //   stateControl4(photosynthesis(0));
+  //   stateControl4(giveF(0));
+  //   expect(stateControl4()).toEqual(x);
+  // });
 });
--- src/js/powerplant.js
+++ src/js/powerplant.js
@@ -1,14 +1,17 @@
 //business logic
-export const storeState = () => {
-  let currentState = {};
+export const storeState = (initialState) => {
+  let currentState = initialState;
   return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState);
     currentState = {...newState};
     return newState;
   };
 };
-
-export const stateControl = storeState();
+//plant = { light: 0};
+//export const tinyPlant = storeState(plant2);
+export const stateControl2 = storeState();
+export const stateControl3 = storeState();
+export const stateControl4 = storeState();
 
 // prop = property to change
 // value = amount to change by
@@ -22,6 +25,15 @@ export const changeState = (prop) => {
   };
 };
 
+export const setState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : value
+    });
+  };
+};
+
 // let plant = { soil: 0, light: 0, water: 0, attack: 0, health: 0 };
 
 // 
@@ -29,10 +41,11 @@ export const photosynthesis = changeState("light");
 export const feedWater = changeState("water");
 // give fertilizer
 export const giveF = changeState("soil");
-export const setAttack = changeState("attack");
-export const setHealth = changeState("health");
+export const damage = changeState("health");
 
-
+export const setAttack = setState("attack");
+export const setHealth = setState("health");
+export const setName = setState("name");
 /*
 Make sure to include tests, to separate your logic, and to use webpack. Once you have completed the example shown in the coursework, try adding the following functionality.
 
